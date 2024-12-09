6cff9f74d6c4e0f9ccf46a61375f3e34e1b340c7
程云天 <yuntianchengbest@outlook.com>
2024-12-09 14:33:17 +0800

    实现aes256的生成密钥，加解密功能

--- libs/encrypt/src/lib.rs
+++ libs/encrypt/src/lib.rs
@@ -1,7 +1,9 @@
+use std::error::Error;
+
 mod aes256;
 
 pub trait EncryptAlgorithm {
-    fn derive_key_from(&self, password: &str) -> Vec<u8>;
+    fn derive_key_from(&self, password: &str, salt: &[u8]) -> Result<Vec<u8>, Box<dyn Error>>;
     fn encrypt(&self, plaintext: &str) -> String;
     fn decrypt(&self, ciphertext: &str) -> String;
 }
